9e72c6dc5f0a2847b7d88b78ee5dd6dbe460a1a1
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2025-01-20 14:24:41 +0200

    part1.8done

--- part01-08_name_and_address/src/name_and_address.py
+++ part01-08_name_and_address/src/name_and_address.py
@@ -1,1 +1,8 @@
 # Write your solution here
+GivenName = input("Given name:")
+FamilyName = input("Family name:")
+StreetAddress = input("Street address:")
+CityandPostalCode = input("City and postal code:")
+print(f"{GivenName} {FamilyName}")
+print(StreetAddress)
+print(CityandPostalCode)
