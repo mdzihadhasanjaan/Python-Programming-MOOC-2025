b1326d2bf88a4edad3cfdbc4d77a702d3e881e4e
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2025-01-25 05:32:37 +0200

    part1.11done

--- part01-11_arithmetics/src/arithmetics.py
+++ part01-11_arithmetics/src/arithmetics.py
@@ -1,3 +1,12 @@
 # Write your solution here
 x = 27
 y = 15
+
+sum = x + y
+sub = x - y
+mul = x * y
+div = x / y
+print(f"{x} + {y} = {sum}")
+print(f"{x} - {y} = {sub}")
+print(f"{x} * {y} = {mul}")
+print(f"{x} / {y} = {div}")
